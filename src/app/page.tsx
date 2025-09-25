6576b58970c84ad940f737285571c4cd14419dad
douglasfcsoares <douglasfcsoares@gmail.com>
2025-09-24 22:08:54 -0300

    chore: add update shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
 
 export default function Home() {
   return (
     <div>
-      <Button className="my-1 bg-red-500 px-5">Bootcamp</Button>
+      <Button className="my-1 px-5">Bootcamp</Button>
     </div>
   );
 }
